be4ef757317b38744276338273f1400ed3d0b19d
Carlos <carlos43@foxmail.com>
2020-03-26 15:34:49 +1300

    Add model user and article

--- blog/database/migrations/create_blogs.php
+++
@@ -1,7 +0,0 @@
-<?php
-Schema::create('blogs', function (Blueprint $table) {
-    $table->increments('id');
-    $table->string('title');
-    $table->text('content');
-    $table->timestamps();
-});
